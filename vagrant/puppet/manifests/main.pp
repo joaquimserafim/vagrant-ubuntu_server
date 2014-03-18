c177d0944021146a9ed80165530791d7e65b56cd
Joaquim Serafim <joaquim.serafim@gmail.com>
2014-03-18 17:32:25 +0000

    add a simple manifest template

--- vagrant/puppet/manifests/main.pp
+++ vagrant/puppet/manifests/main.pp
@@ -6,53 +6,11 @@ class apt_update {
 }
 
 class othertools {
-    package { "git":
-        ensure => latest,
-        require => Exec["aptGetUpdate"]
-    }
-
-    package { "vim-common":
-        ensure => latest,
-        require => Exec["aptGetUpdate"]
-    }
-
     package { "curl":
         ensure => present,
         require => Exec["aptGetUpdate"]
     }
-
-    package { "htop":
-        ensure => present,
-        require => Exec["aptGetUpdate"]
-    }
-
-    package { "g++":
-        ensure => present,
-        require => Exec["aptGetUpdate"]
-    }
-
-    package { "libpq-dev":
-        ensure => present,
-        require => Exec["aptGetUpdate"]
-    }
 }
 
-class node-js {
-  include apt
-  apt::ppa {
-    'ppa:chris-lea/node.js': notify => Package["nodejs"]
-  }
-
-  package { "nodejs" :
-      ensure => latest,
-      require => [Exec["aptGetUpdate"],Class["apt"]]
-  }
-
-  exec { "npm-update" :
-      cwd => "/vagrant",
-      command => "npm -g update",
-      onlyif => ["test -d /vag rant/node_modules"],
-      path => ["/bin", "/usr/bin"],
-      require => Package['nodejs']
-  }
-}
+#include apt_update
+#include othertools
